e136923ab31a6edbc201a3149fa1c9862ac3048a
DeepthiReddy123 <deepthi.gb@ltts.com>
2021-05-29 08:57:35 +0530

    commit

--- Activity3/Activity3-1/src/Vehicle.java
+++ Activity3/Activity3-1/src/Vehicle.java
@@ -1,4 +1,12 @@
 
 public class Vehicle {
+	
+	private String make;// – of type String vehicleNumber – of type String
+
+	private String fuelType;// – of type String
+
+	private int fuelCapacity;// - of type Integer
+
+	private int cc;// – of type Integer
 
 }
