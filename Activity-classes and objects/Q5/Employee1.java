176f85fcb4517186765916129a3ec003ecc7cc71
DeepthiReddy123 <deepthi.gb@ltts.com>
2021-05-31 17:30:49 +0530

    COMMIT

---
+++ Activity-classes and objects/Q5/Employee1.java
@@ -0,0 +1,33 @@
+
+public class Employee1  {
+	
+	    private String name;
+	    private String address;
+	    private String mobile;
+	    
+	    public String getname(){
+	        return name;
+	    }
+
+	    public String getaddress(){
+	        return address;
+	    }
+
+	    public String getmobile(){
+	        return mobile;
+	    }
+
+	    public void setname(String newValue){
+	        name = newValue;
+	    }
+
+	    public void setaddress(String newValue){
+	        address = newValue;
+	    }
+
+	    public void setmobile(String newValue){
+	        mobile = newValue;
+	    }
+	}
+	
+
